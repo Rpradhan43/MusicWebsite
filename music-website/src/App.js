dadd47cb7e1ce3258f033d444298c45389e82726
DanielWright16 <148916585+DanielWright16@users.noreply.github.com>
2024-05-10 13:02:18 -0700

    Added 4 Artists to  App.js

--- music-website/src/App.js
+++ music-website/src/App.js
@@ -33,18 +33,47 @@ function App() {
                 { id: 3, name: 'test', url: '/Keshi/The_reaper.mp3', album: 'The Reaper', dateAdded: 'Jan 10, 2024' },
                 { id: 4, name: 'test', url: '/Keshi/Skeletons.mp3', album: 'Skeletons', dateAdded: 'Feb 15, 2024' },
             ]);
-        } else if (artist === 'Artist_3') {
+        } else if (artist === 'Abba') {
             setCurrentArtist({
-                name: 'Keshi',
-                imageUrl: '/path/to/keshi-image.jpg', // Update with correct image path
-                listeners: '25,000,000'
+                name: 'Abba',
+                imageUrl: '/Artists/Abba/abba.jpg', 
+                listeners: '31,500,000'
             });
             setTracks([
-                { id: 3, name: 'test', url: '/tracks/test.mp3', album: 'The Reaper', dateAdded: 'Jan 10, 2024' },
-                { id: 4, name: 'test', url: '/tracks/test.mp3', album: 'Skeletons', dateAdded: 'Feb 15, 2024' },
+                { id: 5, name: 'Dancing Queen', url: '/Artists/Abba/dancingqueen.mp3', album: 'Dancing Queen', dateAdded: 'Aug 10, 1976' },
+                { id: 6, name: 'Gimme! Gimme! Gimme!', url: '/Artists/Abba/gimmegimmegimme.mp3', album: 'The King Has Lost His Crown', dateAdded: 'Oct 12, 1979' },
             ]);
-        }
-        
+        } else if (artist === 'Bob Marley') {
+            setCurrentArtist({
+                name: 'Bob Marley',
+                imageUrl: '/Artists/BobMarley/bobmarley.jpg', 
+                listeners: '308,600'
+            });
+            setTracks([
+                { id: 7, name: 'Jamming', url: '/Artists/BobMarley/jamming.mp3', album: 'Exodus', dateAdded: 'Jun 3, 1977' },
+                { id: 8, name: 'Waiting In Vain', url: '/Artists/BobMarley/waitinginvain.mp3', album: 'Exodus', dateAdded: 'Jun 3, 1977' },
+            ]);
+        } else if (artist === 'Earth Wind and Fire') {
+            setCurrentArtist({
+                name: 'Earth Wind and Fire',
+                imageUrl: '/Artists/EarthWindAndFire/earthwindandfire.jpg', 
+                listeners: '22,500,000'
+            });
+            setTracks([
+                { id: 9, name: 'Boogie Wonderland', url: '/Artists/EarthWindAndFire/boogiewonderland.mp3', album: 'I Am', dateAdded: 'Jun 9, 1979' },
+                { id: 10, name: 'September', url: '/Artists/EarthWindAndFire/september.mp3', album: 'September', dateAdded: 'Jun 15, 1978' },
+            ]);
+        } else if (artist === 'UB40') {
+            setCurrentArtist({
+                name: 'UB40',
+                imageUrl: '/Artists/UB40/ub40.jpg', 
+                listeners: '9,200,000'
+            });
+            setTracks([
+                { id: 11, name: 'Kingston Town', url: '/Artists/UB40/kiingstontown.mp3', album: 'Labour of Love II', dateAdded: 'Nov 27, 1989' },
+                { id: 12, name: 'Red, Red Wine', url: '/Artists/UB40/redredwine.mp3', album: 'Red, Red Wine', dateAdded: 'Aug 8, 1983' },
+            ]);
+        } 
     };
 
 
